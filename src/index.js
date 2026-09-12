e16b86419ffa3878b80d50343db02b674a8815a1
Mohit Srivastava <mohit@example.com>
2026-09-12 16:55:53 +0530

    Fix popup runtime and route compatibility

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {BrowserRouter} from 'react-router-dom'
 
 import App from './App'
 
-window.process = window.process || {env: {NODE_ENV: 'development'}}
+globalThis.process = globalThis.process || {env: {NODE_ENV: 'development'}}
 
 ReactDOM.render(
   <React.StrictMode>
